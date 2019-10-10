4df0c2043b092445f2fec21dd3410c11015f2809
WebDevIsac <isaclarsson98@gmail.com>
2019-10-10 10:50:00 +0200

    Add p1 & p2 controls for arduino

---
+++ controls_arduino_p2/controls_arduino_p2.ino
@@ -0,0 +1,113 @@
+// The SFE_LSM9DS1 library requires both Wire and SPI be included BEFORE including the 9DS1 library.
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+
+//////////////////////////
+// LSM9DS1 Library Init //
+//////////////////////////
+// Use the LSM9DS1 class to create an object. [imu] can be
+// named anything, we'll refer to that throught the sketch.
+LSM9DS1 imu;
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M  0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+////////////////////////////
+// Sketch Output Settings //
+////////////////////////////
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+static unsigned long lastPrint = 0; // Keep track of print time
+
+//Function definitions
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
+
+void setup() 
+{
+  Serial.begin(230400);
+  
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will work for an out of the box LSM9DS1 Breakout, but may need to be modified if the board jumpers are.");
+    while (1);
+  }
+  pinMode(pushButton, INPUT);
+}
+
+void loop()
+{
+  // Update the sensor values whenever new data is available
+  if ( imu.gyroAvailable() )
+  {
+    // To read from the gyroscope,  first call the
+    // readGyro() function. When it exits, it'll update the
+    // gx, gy, and gz variables with the most current data.
+    imu.readGyro();
+  }
+  if ( imu.accelAvailable() )
+  {
+    // To read from the accelerometer, first call the
+    // readAccel() function. When it exits, it'll update the
+    // ax, ay, and az variables with the most current data.
+    imu.readAccel();
+  }
+  
+  if ((lastPrint + PRINT_SPEED) < millis())
+  {
+    printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+    lastPrint = millis(); // Update lastPrint time
+  }
+
+}
+
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+  int buttonState = digitalRead(pushButton);
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+  
+  // Convert everything from radians to degrees:
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+
+  if (buttonState == 1) {
+    // print out the state of the button:
+    Serial.println("BOMB");
+  }
+
+  if (pitch > 30) {
+    Serial.println("UP");
+  }
+  if (pitch < -30) {
+    Serial.println("DOWN");
+  }
+
+  if (roll > 30) {
+    Serial.println("LEFT");
+  }
+  if (roll < -30) {
+    Serial.println("RIGHT");
+  }
+
+  delay(200);
+}
